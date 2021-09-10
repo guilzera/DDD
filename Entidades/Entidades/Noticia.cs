3fe6027b478d60fe531822df1feda75f86745be5
guilzera <guiguidefarias@hotmail.com>
2021-09-10 20:41:33 -0300

    Criado a tabela de notificação, criado um objeto tipo enum de usuário, Identity de usuarios, objeto noticia.

---
+++ Entidades/Entidades/Noticia.cs
@@ -0,0 +1,29 @@
+﻿using Entidades.Notificacoes;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entidades.Entidades
+{
+    public class Noticia : Notifica
+    {
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        public string Titulo { get; set; }
+
+        [MaxLength(255)]
+        public string Informacao { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+    }
+}
---
+++ Entidades/Notificacoes/Notifica.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entidades.Notificacoes
+{
+    public class Notifica
+    {
+        [NotMapped]
+        public string NomePropriedade { get; set; } //Nome do erro
+
+        [NotMapped]
+        public string Mensagem { get; set; } //Mensagem do erro
+
+        [NotMapped]
+        public List<Notifica> Notificacoes; //Lista de erros de validações
+
+        public Notifica()
+        {
+            Notificacoes = new List<Notifica>();
+        }
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Campo obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+                return false;
+            }
+            return true;
+        }
+
+        public bool ValdiarPropriedadeDecimal(decimal valor, string nomePropriedade)
+        {
+            if(valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Valor deve ser maior que 0",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+            return true;
+        }
+    }
+}
